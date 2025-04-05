d14bfaa8a33016fa86581c740ae9597b5b09385e
GabbaMendonca <gabba.kumbuca@gmail.com>
2025-04-05 04:05:13 -0300

    ✅ rapid test scraping farol

--- gabbarito/main.py
+++ gabbarito/main.py
@@ -8,6 +8,15 @@ def main():
     driver = chrome.get_chrome_driver()
     farol.open_farol(driver)
     Prompt.ask(
+        "Press Enter to continue after the page has loaded.",
+        default="",
+        show_default=False,
+    )
+    data = farol.scrape_farol(driver)
+    from pprint import pprint
+
+    pprint(data)
+    Prompt.ask(
         "Press Enter to close the browser and exit the program.",
         default="",
         show_default=False,
